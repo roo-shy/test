9f25b378e907ca5af712c0a3948e235ce8d3a2f5
at-rooshee <rushania@youcanbook.me>
2018-02-03 19:18:33 -0600

    Adding test files with reviewing material

---
+++ index.js
@@ -0,0 +1,68 @@
+// JS Data Types
+
+/*
+* Boolean
+* Null - empty object
+* Undefined
+* Number
+* String
+* Symbol
+*/
+
+// Boolean
+// var num1 = "100";
+// var num2 = 100;
+// boolNum = num1 === num2;
+// alert(typeof boolNum); //boolean
+// alert(num1 === num2); //false
+
+
+// Number
+// var integerNumber = 100;
+// var floatingPointNumber = 100.50;
+// var float2 = 100.0;
+// 
+// console.log(integerNumber, floatingPointNumber, float2);
+
+// alert(Number.MAX_VALUE);
+// alert(Number.MIN_VALUE);
+
+// NaN
+// alert(NaN === NaN); //false
+// isNaN
+// alert(isNaN(NaN)); //true
+// alert(isNaN('100')); //false
+// alert(isNaN(100)); //false
+// alert(isNaN('blue'));
+// alert(isNaN(true)); // true converts to 1 & false converts to 0
+
+// var num2 = parseInt("1234iehfiefieh");
+// alert(num2);
+
+// var num3 = parseFloat("20.69");
+// alert(num3);
+
+//String
+
+// var myName = 'Mozarella';
+// console.log(myName.length);
+// 
+// var age = 30;
+// var ageStringify = age.toString();
+// console.log(typeof ageStringify);
+
+// Objects
+
+var woman = {
+  name: 'Olga',
+  age: 50,
+  sayname: function() {
+    return this.name
+  }
+}
+
+console.log(woman.name, woman['age'], woman.sayname());
+
+
+
+

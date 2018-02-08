feb388f40d4d01d4d46c810afe0199838b9db0aa
at-rooshee <rushania@youcanbook.me>
2018-02-08 10:19:38 -0600

    Adding more info

--- index.js
+++ index.js
@@ -52,17 +52,137 @@
 // console.log(typeof ageStringify);
 
 // Objects
+/* creating object - {}, new
+accessing - . []
+accessing object propery with variable
+updating object properties
+adding new properties
+deleting properties
+object hasOwnProperty
+nested oject
+*/
 
 // var woman = {
 //   name: 'Olga',
 //   age: 50,
 //   sayname: function() {
 //     return this.name
+//   },
+//   likes: {
+//     cartoon: {
+//       one: "Hobby Kids",
+//       two: "Chickens"
+//     },
+//     cookie: "Chocolate Chip"
 //   }
 // }
+// woman.name = "Zelda";
+// woman.address = "US";
 // 
-// console.log(woman.name, woman['age'], woman.sayname());
+// delete woman.address;
+// 
+// console.log("Has this property? ", woman.hasOwnProperty("age"));
+// 
+// console.log(woman['age'], woman.sayname(), woman.likes['cartoon'].one);
 
+// Operators
 
+/*
+* unary operators ++ --
+* boolean operators - logical ! && ||
+* multiplcative - * / %
+* additive +
+* relational < > <= >=
+* equality  == === != !==
+* conditional - ternary ---- (var = expression ? true : false)
+* assignment = += -= *= /= %=
+* comma
+*/
+
+// var date1 = 10;
+// var date2 = 18;
+// 
+// var test = date1 > date2 ? "Correct" : "Wrong";
+// 
+// console.log(test); // ternarary
+// 
+// var num = 10;
+// num = num + 10;
+// num += 10;
+// console.log(num); // compound assignment
+// 
+// var name = 'R';
+// var age = 29;
+// var school = 'secondary';
+// 
+// var name = 'R', age = 29, school = 'secondary'; // comma operators
+
+/* Statements:
+
+if else
+do while
+while
+for
+for in
+labeled
+break continue
+switch
+*/
+// if - flow-control Statement
+// if statement evaluates a given condition to a Boolean true or false
+
+// var num = 100;
+// 
+// if(num < 50) {
+//   alert('Yes it is greater than ' + num);
+// } else  {
+//   alert('It is not');
+// }
+
+// var n = 26;
+// 
+// if(n >= 27) {
+//   alert ('greater')
+// } else if (n <= 25) {
+//   alert ('again not')
+//   } else  {
+//     alert ('number is 26')
+//   }
+  
+// var num = 0;
+
+// do {
+//   num++;
+//   console.log('Number:', num);
+// } while (num < 10);
+
+// while(num < 10) {
+//   num++;
+//   console.log('Number:', num);
+// }
+
+// for (var n = 0; n < 10; n++) {
+//   alert(n);
+// }
+
+// var person = {
+//   name: 'Roo',
+//   age: 30,
+//   planet: 'Earth'
+// };
+
+// for(item in window) {
+//   console.log(item);
+// }
+
+var num = 0;
+
+for(var n = 0; n < 10; n++) {
+  if (n === 5) {
+    break;// continue
+  }
+  num++;
+}
+console.log(num);
 
 
